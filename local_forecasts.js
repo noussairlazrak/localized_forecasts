6d282043da93c281308c3bd23f4166c6a723f78a
NoussairLazrak <lazrak.noussair@gmail.com>
2024-10-13 23:51:02 +0100

    Update local_forecasts.js

--- local_forecasts.js
+++ local_forecasts.js
@@ -483,19 +483,20 @@ function create_map(sites, param) {
       map.on('click', 'unclustered-point', (e) => {
         const coordinates = e.features[0].geometry.coordinates.slice();
         const location_id = e.features[0].properties.location_id;
-    
-
-        const location_name = e.features[0].properties.location_name;
-        console.log("Location Name:", location_name); 
-        const sanitizedLocationName = cleanText(location_name); 
-    
-        const status = e.features[0].properties.status;
+        const location_name = e.features[0].properties.location_name.replace(/[^a-z0-9\s]/gi, '_').replace(/[_\s]/g, '_');
         const observation_source = e.features[0].properties.observation_source;
         const observation_value = e.features[0].properties.forecasted_value;
     
-        const precomputed_forecasts = e.features[0].properties.precomputed_forecasts ? $.parseJSON(e.features[0].properties.precomputed_forecasts) : [];
-        const obs_option = e.features[0].properties.obs_options ? $.parseJSON(e.features[0].properties.obs_options) : [];
-        const observation_unit = obs_option.length > 0 ? obs_option[0].no2.unit : 'N/A'; 
+        console.log("loading..");
+        
+        const anchorSelector = `.launch-local-forecasts[station_id="${location_id}"][parameter="no2"][location_name="${location_name}"]`;
+    
+
+        if ($(anchorSelector).length) {
+            $(anchorSelector).click();
+        } else {
+            console.error("Anchor element not found:", anchorSelector);
+        }
     
 
         while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
@@ -505,20 +506,8 @@ function create_map(sites, param) {
         new mapboxgl.Popup()
             .setLngLat(coordinates)
             .setHTML(
-                `location_id: ${location_id}<br>Was there a location_name?: ${sanitizedLocationName}`
+                `location_id: ${location_id}<br>Location Name: ${location_name}`
             )
-            .on('open', () => {
-                openForecastsWindow(
-                    ["Loading", "Please hold"], 
-                    location_id, 
-                    'no2', 
-                    sanitizedLocationName, 
-                    observation_value || 'N/A', 
-                    observation_unit || 'N/A', 
-                    observation_source || 'N/A', 
-                    precomputed_forecasts.length > 0 ? precomputed_forecasts[0].no2.forecasts : []
-                );
-            })
             .addTo(map);
     });
 
